fb193f9db0f63e55c26ae4c6e6b654d62bfc1f02
Laura Beegle <laura@beegle.org>
2013-02-07 16:03:14 -0500

    Added some records.

--- distpong.erl
+++ distpong.erl
@@ -1,5 +1,14 @@
+% Start one node as "erl -sname one".
+% Start second node as "erl -sname two".
+%
+% On two, compile and do distpong:start_pong().
+% On one, compile and do distpong:start_ping(two@xxx).
+% where xxx is the node name (like xubuntu12box or guardian).
+
 -module(distpong).
 -export([start_ping/1, start_pong/0, ping/2, pong/0]).
+
+-record(spin,{name, age, word}).
 
 ping(0, Pong_Node) ->
     {pong, Pong_Node} ! finished,
@@ -9,9 +18,11 @@ ping(N, Pong_Node) ->
     {pong, Pong_Node} ! {ping, self()},
     receive
         pong ->
-            case N rem 2 of
+            case N rem 3 of
                 0 -> {pong, Pong_Node} ! {grumble, "Sally"};
-                1 -> noop
+		1 -> {pong, Pong_Node} ! #spin{name="Bob", age="37", word="Awesome!"};
+		2 -> {pong, Pong_Node} ! #spin{name="June", word="Cool"}
+%                1 -> noop
             end,
             io:format("Ping!~n", [])
     end,
@@ -24,15 +35,24 @@ pong() ->
         {grumble, Name} ->
             io:format("*** Grumble, grumble, ~p! ***~n", [Name]),
             pong();
+	#spin{name="Bob"} ->
+	    io:format("Special Bob Spin ~p~n", [age]),
+	    pong();
+	{spin, Name, Age, Word} ->
+	    io:format("SPINNING! ~p ~p ~p~n", [Name, Age, Word]),
+	    pong();
         {ping, Ping_PID} ->
             io:format("Pong!~n", []),
             Ping_PID ! pong,
             pong()
+% If don't receive any messages after 5 seconds, shutdown
+%    after 5000 ->
+%	    io:format("Pong timed out.")
     end.
 
 start_pong() ->
     register(pong, spawn(distpong, pong, [])).
 
 start_ping(Pong_Node) ->
-    Num_Hits = 3,
+    Num_Hits = 5,
     spawn(distpong, ping, [Num_Hits, Pong_Node]).
